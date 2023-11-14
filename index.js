19313847d64a71fa4fcb750d456a25f45fd269fc
Seth Gartz <sgartz@devpipeline.com>
2023-11-14 12:14:50 -0600

    few touchups

--- index.js
+++ index.js
@@ -1,11 +1,1 @@
-const addToList = document.getElementById("add-input");
-const submitButton = document.querySelector(".submit-button");
-const todoList = document.getElementById("todo-list");
-const completedList = document.getElementById("completed-list");
-const completedMessage = document.getElementById("completed-message");
 
-submitButton.addEventListener("click", () => {
-  const listItem = addToList.value;
-  if (listItem != "") {
-  }
-});
